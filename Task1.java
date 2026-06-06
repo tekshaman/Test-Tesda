3c0f950d65a96546c17d6ff54dacfce8a57a2896
teksman <myprivateit@gmail.com>
2026-06-06 16:22:16 +0800

    Task1 HelloWorld

---
+++ Task1.java
@@ -0,0 +1,13 @@
+/** 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+public class Task1 {
+
+ 
+ //-> mistake helloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+
+}
